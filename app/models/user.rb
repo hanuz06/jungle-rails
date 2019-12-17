ae294d8791e9488fcafcdecb0541f2ee3af627b5
hanuz06 <lee1975uz@gmail.com>
2019-12-17 18:41:09 +0000

    add rspec for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ActiveRecord::Base
 
   has_secure_password
+
+   validates :password, presence: true, length: { in: 5..10 }  
+   validates :password_confirmation, presence: true
+   validates :email, presence: true, uniqueness: true, confirmation: { case_sensitive: false }
+   validates :first_name, presence: true
+   validates :last_name, presence: true
+
+   def authenticate_with_credentials(email, password)
+
+    @user =  User.find_by_email(email)
+
+    @user if @user && @user.authenticate(password)
+  
+  end
   
 end
