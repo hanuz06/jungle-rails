b928d688fde4392fef765261d91a09c3674f954c
hanuz06 <lee1975uz@gmail.com>
2019-12-18 05:21:34 +0000

    amend session login and authentication method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,8 @@ class User < ActiveRecord::Base
    validates :first_name, presence: true
    validates :last_name, presence: true
 
-   def authenticate_with_credentials(email, password)
-    new_email = email.strip.downcase
-    #email.downcase
+   def self.authenticate_with_credentials(email, password)   
+    new_email = email.downcase.strip
     
     @user =  User.find_by_email(new_email)
 
